df9eb82bcf8da889134a2f73d6c37624a2799e91
Bernd Konnerth <bernd.konnerth@atos.net>
2018-12-22 21:07:54 +0100

    Added Sure frequency bands

--- main.js
+++ main.js
@@ -135,7 +135,111 @@ function createWindow () {
                     { label: 'EM (470 - 798MHz)', click () { wc.send ( 'CHANGE_BAND', { start_freq: 470000, stop_freq: 798000, label: 'Sennheiser Digital 9000 EM (470 - 798MHz)', band: 'SEN_EM_9000' } ); } }
                 ]},
             ]},
-            { label:'Shure', click() { } },
+            { label:'Shure', submenu: [ 
+                { label:'BLX (Analog)', submenu: [
+                    { label: 'H8E-Band (518 - 542MHz)', click () { wc.send ( 'CHANGE_BAND', { start_freq: 518000, stop_freq: 542000, label: 'Sure BLX H8E-Band (518 - 542MHz)', band: 'SUR_H8E' } ); } },
+                    { label: 'K3E-Band (606 - 630MHz)', click () { wc.send ( 'CHANGE_BAND', { start_freq: 606000, stop_freq: 630000, label: 'Sure BLX K3E-Band (606 - 630MHz)', band: 'SUR_K3E' } ); } },
+                    { label: 'K14-Band (614 - 638MHz)', click () { wc.send ( 'CHANGE_BAND', { start_freq: 614000, stop_freq: 638000, label: 'Sure BLX K14-Band (614 - 638MHz)', band: 'SUR_K14' } ); } },
+                    { label: 'M17-Band (662 - 686MHz)', click () { wc.send ( 'CHANGE_BAND', { start_freq: 662000, stop_freq: 686000, label: 'Sure BLX M17-Band (662 - 686MHz)', band: 'SUR_M17' } ); } },
+                    { label: 'Q25-Band (742 - 766MHz)', click () { wc.send ( 'CHANGE_BAND', { start_freq: 742000, stop_freq: 766000, label: 'Sure BLX Q25-Band (742 - 766MHz)', band: 'SUR_Q25' } ); } },
+                    { label: 'R12-Band (794 - 806MHz)', click () { wc.send ( 'CHANGE_BAND', { start_freq: 794000, stop_freq: 806000, label: 'Sure BLX R12-Band (794 - 806MHz)', band: 'SUR_R12' } ); } },
+                    { label: 'S8-Band  (823 - 832MHz)', click () { wc.send ( 'CHANGE_BAND', { start_freq: 823000, stop_freq: 832000, label: 'Sure BLX  S8-Band (823 - 832MHz)', band: 'SUR_S8'  } ); } },
+                    { label: 'T11-Band (863 - 865MHz)', click () { wc.send ( 'CHANGE_BAND', { start_freq: 863000, stop_freq: 865000, label: 'Sure BLX T11-Band (863 - 865MHz)', band: 'SUR_T11' } ); } }
+                ]},
+                { label:'SLX (Analog)', submenu: [
+                    { label: 'G4E-Band (470 - 494MHz)', click () { wc.send ( 'CHANGE_BAND', { start_freq: 470000, stop_freq: 494000, label: 'Sure SLX G4E-Band (470 - 494MHz)', band: 'SUR_G4E' } ); } },
+                    { label: 'G5E-Band (494 - 518MHz)', click () { wc.send ( 'CHANGE_BAND', { start_freq: 494000, stop_freq: 518000, label: 'Sure SLX G5E-Band (494 - 518MHz)', band: 'SUR_G5E' } ); } },
+                    { label: 'H5-Band  (518 - 542MHz)', click () { wc.send ( 'CHANGE_BAND', { start_freq: 518000, stop_freq: 542000, label: 'Sure SLX H5-Band  (518 - 542MHz)', band: 'SUR_H5'  } ); } },
+                    { label: 'J3-Band  (572 - 596MHz)', click () { wc.send ( 'CHANGE_BAND', { start_freq: 572000, stop_freq: 596000, label: 'Sure SLX J3-Band  (572 - 596MHz)', band: 'SUR_J3'  } ); } },
+                    { label: 'K3E-Band (606 - 630MHz)', click () { wc.send ( 'CHANGE_BAND', { start_freq: 606000, stop_freq: 630000, label: 'Sure SLX K3E-Band (606 - 630MHz)', band: 'SUR_K3E' } ); } },
+                    { label: 'L4E-Band (638 - 662MHz)', click () { wc.send ( 'CHANGE_BAND', { start_freq: 638000, stop_freq: 662000, label: 'Sure SLX L4E-Band (638 - 662MHz)', band: 'SUR_L4E' } ); } },
+                    { label: 'P4-Band  (702 - 726MHz)', click () { wc.send ( 'CHANGE_BAND', { start_freq: 702000, stop_freq: 726000, label: 'Sure SLX  P4-Band (702 - 726MHz)', band: 'SUR_P4'  } ); } },
+                    { label: 'Q24-Band (736 - 754MHz)', click () { wc.send ( 'CHANGE_BAND', { start_freq: 736000, stop_freq: 754000, label: 'Sure SLX Q24-Band (736 - 754MHz)', band: 'SUR_Q24' } ); } },
+                    { label: 'R5-Band  (800 - 820MHz)', click () { wc.send ( 'CHANGE_BAND', { start_freq: 800000, stop_freq: 820000, label: 'Sure SLX  R5-Band (800 - 820MHz)', band: 'SUR_R5'  } ); } },
+                    { label: 'S10-Band (823 - 832MHz)', click () { wc.send ( 'CHANGE_BAND', { start_freq: 823000, stop_freq: 832000, label: 'Sure SLX S10-Band (823 - 832MHz)', band: 'SUR_S10' } ); } },
+                    { label: 'S6-Band  (838 - 865MHz)', click () { wc.send ( 'CHANGE_BAND', { start_freq: 838000, stop_freq: 865000, label: 'Sure SLX  S6-Band (838 - 865MHz)', band: 'SUR_S6'  } ); } }
+                ]},
+                { label:'GLX-D (Digital)', submenu: [
+                    { label: 'Z2-Band (2404 - 2478MHz)', click () { wc.send ( 'CHANGE_BAND', { start_freq: 2404000, stop_freq: 2478000, label: 'Sure GLX-D Z2-Band (2404 - 2478MHz)', band: 'SUR_Z2' } ); } }
+                ]},
+                { label:'FP (Analog)', submenu: [
+                    { label: 'G4E-Band (470 - 494MHz)', click () { wc.send ( 'CHANGE_BAND', { start_freq: 470000, stop_freq: 494000, label: 'Sure SLX G4E-Band (470 - 494MHz)', band: 'SUR_G4E' } ); } },
+                    { label: 'G5E-Band (494 - 518MHz)', click () { wc.send ( 'CHANGE_BAND', { start_freq: 494000, stop_freq: 518000, label: 'Sure SLX G5E-Band (494 - 518MHz)', band: 'SUR_G5E' } ); } },
+                    { label: 'H5-Band  (518 - 542MHz)', click () { wc.send ( 'CHANGE_BAND', { start_freq: 518000, stop_freq: 542000, label: 'Sure SLX H5-Band  (518 - 542MHz)', band: 'SUR_H5'  } ); } },
+                    { label: 'J3-Band  (572 - 596MHz)', click () { wc.send ( 'CHANGE_BAND', { start_freq: 572000, stop_freq: 596000, label: 'Sure SLX J3-Band  (572 - 596MHz)', band: 'SUR_J3'  } ); } },
+                    { label: 'K3E-Band (606 - 630MHz)', click () { wc.send ( 'CHANGE_BAND', { start_freq: 606000, stop_freq: 630000, label: 'Sure SLX K3E-Band (606 - 630MHz)', band: 'SUR_K3E' } ); } },
+                    { label: 'L4E-Band (638 - 662MHz)', click () { wc.send ( 'CHANGE_BAND', { start_freq: 638000, stop_freq: 662000, label: 'Sure SLX L4E-Band (638 - 662MHz)', band: 'SUR_L4E' } ); } },
+                    { label: 'P4-Band  (702 - 726MHz)', click () { wc.send ( 'CHANGE_BAND', { start_freq: 702000, stop_freq: 726000, label: 'Sure SLX  P4-Band (702 - 726MHz)', band: 'SUR_P4'  } ); } },
+                    { label: 'Q24-Band (736 - 754MHz)', click () { wc.send ( 'CHANGE_BAND', { start_freq: 736000, stop_freq: 754000, label: 'Sure SLX Q24-Band (736 - 754MHz)', band: 'SUR_Q24' } ); } },
+                    { label: 'S6-Band  (838 - 865MHz)', click () { wc.send ( 'CHANGE_BAND', { start_freq: 838000, stop_freq: 865000, label: 'Sure SLX  S6-Band (838 - 865MHz)', band: 'SUR_S6'  } ); } }
+                ]},
+                { label:'ULX-D (Digital)', submenu: [
+                    { label: 'V51-Band (174 - 216MHz)', click () { wc.send ( 'CHANGE_BAND', { start_freq: 174000, stop_freq: 216000, label: 'Sure ULX-D G4E-Band (174 - 216MHz)', band: 'SUR_V51' } ); } },
+                    { label: 'G51-Band (470 - 534MHz)', click () { wc.send ( 'CHANGE_BAND', { start_freq: 470000, stop_freq: 534000, label: 'Sure ULX-D G51-Band (470 - 534MHz)', band: 'SUR_G51' } ); } },
+                    { label: 'H51-Band (534 - 598MHz)', click () { wc.send ( 'CHANGE_BAND', { start_freq: 534000, stop_freq: 598000, label: 'Sure ULX-D H51-Band (534 - 598MHz)', band: 'SUR_H51' } ); } },
+                    { label: 'K51-Band (606 - 670MHz)', click () { wc.send ( 'CHANGE_BAND', { start_freq: 606000, stop_freq: 670000, label: 'Sure ULX-D K51-Band (606 - 670MHz)', band: 'SUR_K51' } ); } },
+                    { label: 'P51-Band (710 - 782MHz)', click () { wc.send ( 'CHANGE_BAND', { start_freq: 710000, stop_freq: 782000, label: 'Sure ULX-D P51-Band (710 - 782MHz)', band: 'SUR_P51' } ); } },
+                    { label: 'Q51-Band (794 - 806MHz)', click () { wc.send ( 'CHANGE_BAND', { start_freq: 794000, stop_freq: 806000, label: 'Sure ULX-D Q51-Band (794 - 806MHz)', band: 'SUR_Q51' } ); } },
+                    { label: 'R51-Band (800 - 810MHz)', click () { wc.send ( 'CHANGE_BAND', { start_freq: 800000, stop_freq: 810000, label: 'Sure ULX-D R51-Band (800 - 810MHz)', band: 'SUR_R51' } ); } },
+                    { label: 'Z17-Band (1492 - 1525MHz)', click () { wc.send ( 'CHANGE_BAND', { start_freq: 1492000, stop_freq: 1525000, label: 'Sure ULX-D Z17-Band (1492 - 1525MHz)', band: 'SUR_Z17' } ); } },
+                    { label: 'Z18-Band (1785 - 1805MHz)', click () { wc.send ( 'CHANGE_BAND', { start_freq: 1785000, stop_freq: 1805000, label: 'Sure ULX-D Z18-Band (1785 - 1805MHz)', band: 'SUR_Z18' } ); } }
+                ]},
+                { label:'QLX-D (Digital)', submenu: [
+                    { label: 'V51-Band (174 - 216MHz)', click () { wc.send ( 'CHANGE_BAND', { start_freq: 174000, stop_freq: 216000, label: 'Sure QLX-D G4E-Band (174 - 216MHz)', band: 'SUR_V51' } ); } },
+                    { label: 'G51-Band (470 - 534MHz)', click () { wc.send ( 'CHANGE_BAND', { start_freq: 470000, stop_freq: 534000, label: 'Sure QLX-D G51-Band (470 - 534MHz)', band: 'SUR_G51' } ); } },
+                    { label: 'H51-Band (534 - 598MHz)', click () { wc.send ( 'CHANGE_BAND', { start_freq: 534000, stop_freq: 598000, label: 'Sure QLX-D H51-Band (534 - 598MHz)', band: 'SUR_H51' } ); } },
+                    { label: 'K51-Band (606 - 670MHz)', click () { wc.send ( 'CHANGE_BAND', { start_freq: 606000, stop_freq: 670000, label: 'Sure QLX-D K51-Band (606 - 670MHz)', band: 'SUR_K51' } ); } },
+                    { label: 'L52-Band (632 - 694MHz)', click () { wc.send ( 'CHANGE_BAND', { start_freq: 632000, stop_freq: 694000, label: 'Sure QLX-D L52-Band (632 - 694MHz)', band: 'SUR_L52' } ); } },
+                    { label: 'P51-Band (710 - 782MHz)', click () { wc.send ( 'CHANGE_BAND', { start_freq: 710000, stop_freq: 782000, label: 'Sure QLX-D P51-Band (710 - 782MHz)', band: 'SUR_P51' } ); } },
+                    { label: 'Q51-Band (794 - 806MHz)', click () { wc.send ( 'CHANGE_BAND', { start_freq: 794000, stop_freq: 806000, label: 'Sure QLX-D Q51-Band (794 - 806MHz)', band: 'SUR_Q51' } ); } },
+                    { label: 'S50-Band (823 - 865MHz)', click () { wc.send ( 'CHANGE_BAND', { start_freq: 823000, stop_freq: 865000, label: 'Sure QLX-D S50-Band (823 - 865MHz)', band: 'SUR_S50' } ); } },
+                    { label: 'Z17-Band (1492 - 1525MHz)', click () { wc.send ( 'CHANGE_BAND', { start_freq: 1492000, stop_freq: 1525000, label: 'Sure QLX-D Z17-Band (1492 - 1525MHz)', band: 'SUR_Z17' } ); } },
+                    { label: 'Z18-Band (1785 - 1805MHz)', click () { wc.send ( 'CHANGE_BAND', { start_freq: 1785000, stop_freq: 1805000, label: 'Sure QLX-D Z18-Band (1785 - 1805MHz)', band: 'SUR_Z18' } ); } }
+                ]},
+                { label:'AXIENT (Digital)', submenu: [
+                    { label: 'A(G56)-Band (470 - 636MHz)', click () { wc.send ( 'CHANGE_BAND', { start_freq: 470000, stop_freq: 636000, label: 'Sure AXIENT A(G56)-Band (470 - 636MHz)', band: 'SUR_A_G56' } ); } },
+                    { label: 'B(K57)-Band (606 - 790MHz)', click () { wc.send ( 'CHANGE_BAND', { start_freq: 606000, stop_freq: 790000, label: 'Sure AXIENT B(K57)-Band (606 - 790MHz)', band: 'SUR_B_K57' } ); } }
+                ]},
+                { label:'MICROFLEX WIRELESS', submenu: [
+                    { label: 'Z11-Band (1880 - 1900MHz)', click () { wc.send ( 'CHANGE_BAND', { start_freq: 1880000, stop_freq: 1900000, label: 'Sure MICROFLEX WIRELESS Z11-Band (1880 - 1900MHz)', band: 'SUR_Z11' } ); } }
+                ]},
+                { label:'PSM 200 (Analog)', submenu: [
+                    { label: 'H2-Band  (518 - 554MHz)', click () { wc.send ( 'CHANGE_BAND', { start_freq: 518000, stop_freq: 554000, label: 'Sure PSM 200 H2-Band  (518 - 554MHz)', band: 'SUR_H2'  } ); } },
+                    { label: 'K9E-Band (606 - 638MHz)', click () { wc.send ( 'CHANGE_BAND', { start_freq: 606000, stop_freq: 638000, label: 'Sure PSM 200 K9E-Band (606 - 638MHz)', band: 'SUR_K9E' } ); } },
+                    { label: 'Q3-Band  (748 - 784MHz)', click () { wc.send ( 'CHANGE_BAND', { start_freq: 748000, stop_freq: 784000, label: 'Sure PSM 200  Q3-Band (748 - 784MHz)', band: 'SUR_Q3'  } ); } },
+                    { label: 'R8-Band  (800 - 814MHz)', click () { wc.send ( 'CHANGE_BAND', { start_freq: 800000, stop_freq: 814000, label: 'Sure PSM 200  R8-Band (800 - 814MHz)', band: 'SUR_R8'  } ); } },
+                    { label: 'S5-Band  (842 - 865MHz)', click () { wc.send ( 'CHANGE_BAND', { start_freq: 842000, stop_freq: 865000, label: 'Sure PSM 200  S5-Band (842 - 865MHz)', band: 'SUR_S5'  } ); } }
+                ]},
+                { label:'PSM 300 (Analog)', submenu: [
+                    { label: 'H8E/H20-Band (518 - 542MHz)', click () { wc.send ( 'CHANGE_BAND', { start_freq: 518000, stop_freq: 542000, label: 'Sure PSM 300 H8E/H20-Band (518 - 542MHz)', band: 'SUR_H8E_H20' } ); } },
+                    { label: 'K3E-Band (606 - 630MHz)', click () { wc.send ( 'CHANGE_BAND', { start_freq: 606000, stop_freq: 630000, label: 'Sure PSM 300 K3E-Band (606 - 630MHz)', band: 'SUR_K3E' } ); } },
+                    { label: 'K12-Band (614 - 638MHz)', click () { wc.send ( 'CHANGE_BAND', { start_freq: 614000, stop_freq: 638000, label: 'Sure PSM 300 K12-Band (614 - 638MHz)', band: 'SUR_K12' } ); } },
+                    { label: 'L19-Band (630 - 654MHz)', click () { wc.send ( 'CHANGE_BAND', { start_freq: 630000, stop_freq: 654000, label: 'Sure PSM 300 L19-Band (630 - 654MHz)', band: 'SUR_L19' } ); } },
+                    { label: 'M16-Band (686 - 710MHz)', click () { wc.send ( 'CHANGE_BAND', { start_freq: 686000, stop_freq: 710000, label: 'Sure PSM 300 M16-Band (686 - 710MHz)', band: 'SUR_M16' } ); } },
+                    { label: 'Q25-Band (742 - 766MHz)', click () { wc.send ( 'CHANGE_BAND', { start_freq: 742000, stop_freq: 766000, label: 'Sure PSM 300 Q25-Band (742 - 766MHz)', band: 'SUR_Q25' } ); } },
+                    { label: 'R12-Band (794 - 806MHz)', click () { wc.send ( 'CHANGE_BAND', { start_freq: 794000, stop_freq: 806000, label: 'Sure PSM 300 R12-Band (794 - 806MHz)', band: 'SUR_R12' } ); } },
+                    { label: 'S8-Band  (823 - 832MHz)', click () { wc.send ( 'CHANGE_BAND', { start_freq: 823000, stop_freq: 832000, label: 'Sure PSM 300  S8-Band (823 - 832MHz)', band: 'SUR_S8'  } ); } },
+                    { label: 'T11-Band (863 - 865MHz)', click () { wc.send ( 'CHANGE_BAND', { start_freq: 863000, stop_freq: 865000, label: 'Sure PSM 300 T11-Band (863 - 865MHz)', band: 'SUR_T11' } ); } }
+                ]},
+                { label:'PSM 900 (Analog)', submenu: [
+                    { label: 'G6E-Band (470 - 506MHz)', click () { wc.send ( 'CHANGE_BAND', { start_freq: 470000, stop_freq: 506000, label: 'Sure PSM 900 G6E-Band (470 - 506MHz)', band: 'SUR_G6E' } ); } },
+                    { label: 'G7E-Band (506 - 542MHz)', click () { wc.send ( 'CHANGE_BAND', { start_freq: 506000, stop_freq: 542000, label: 'Sure PSM 900 G7E-Band (506 - 542MHz)', band: 'SUR_G7E' } ); } },
+                    { label: 'K1E-Band (596 - 632MHz)', click () { wc.send ( 'CHANGE_BAND', { start_freq: 596000, stop_freq: 632000, label: 'Sure PSM 900 K1E-Band (596 - 632MHz)', band: 'SUR_K1E' } ); } },
+                    { label: 'L6E-Band (656 - 692MHz)', click () { wc.send ( 'CHANGE_BAND', { start_freq: 656000, stop_freq: 692000, label: 'Sure PSM 900 L6E-Band (656 - 692MHz)', band: 'SUR_L6E' } ); } },
+                    { label: 'P7-Band  (702 - 742MHz)', click () { wc.send ( 'CHANGE_BAND', { start_freq: 702000, stop_freq: 742000, label: 'Sure PSM 900  P7-Band (702 - 742MHz)', band: 'SUR_P7'  } ); } },
+                    { label: 'Q15-Band (750 - 790MHz)', click () { wc.send ( 'CHANGE_BAND', { start_freq: 750000, stop_freq: 790000, label: 'Sure PSM 900 Q15-Band (750 - 790MHz)', band: 'SUR_Q15' } ); } },
+                    { label: 'R22-Band (790 - 830MHz)', click () { wc.send ( 'CHANGE_BAND', { start_freq: 790000, stop_freq: 830000, label: 'Sure PSM 900 R22-Band (790 - 830MHz)', band: 'SUR_R22' } ); } }
+                ]},
+                { label:'PSM 1000 (Analog)', submenu: [
+                    { label: 'G10E-Band (470 - 542MHz)',click () { wc.send ( 'CHANGE_BAND', { start_freq: 470000, stop_freq: 542000, label: 'Sure PSM 1000 G10E-Band (470 - 542MHz)', band: 'SUR_G10E'} ); } },
+                    { label: 'J8E-Band (554 - 626MHz)', click () { wc.send ( 'CHANGE_BAND', { start_freq: 554000, stop_freq: 662000, label: 'Sure PSM 1000 J8E-Band (554 - 662MHz)' , band: 'SUR_J8E' } ); } },
+                    { label: 'K10E-Band (596 - 668MHz)',click () { wc.send ( 'CHANGE_BAND', { start_freq: 596000, stop_freq: 668000, label: 'Sure PSM 1000 K10E-Band (596 - 668MHz)', band: 'SUR_K10E'} ); } },
+                    { label: 'L8E-Band (626 - 698MHz)', click () { wc.send ( 'CHANGE_BAND', { start_freq: 626000, stop_freq: 698000, label: 'Sure PSM 1000 L8E-Band (626 - 698MHz)' , band: 'SUR_L8E' } ); } },
+                    { label: 'L9E-Band (670 - 742MHz)', click () { wc.send ( 'CHANGE_BAND', { start_freq: 670000, stop_freq: 742000, label: 'Sure PSM 1000 L9E-Band (670 - 742MHz)' , band: 'SUR_L9E' } ); } },
+                    { label: 'P8-Band  (710 - 790MHz)', click () { wc.send ( 'CHANGE_BAND', { start_freq: 710000, stop_freq: 790000, label: 'Sure PSM 1000  P8-Band (710 - 790MHz)' , band: 'SUR_P8'  } ); } },
+                    { label: 'Q22E-Band (750 - 822MHz)', click () { wc.send ( 'CHANGE_BAND', { start_freq: 750000, stop_freq: 822000, label: 'Sure PSM 1000 Q22E-Band (750 - 822MHz)' , band: 'SUR_Q22E' } ); } }
+                ]}
+            ]},
             { type:'separator' },
             { label: 'VHF Band (174 - 230MHz)'   , click () { wc.send ( 'CHANGE_BAND', { start_freq: 174000, stop_freq: 230000, label: 'VHF Band (174 - 230MHz)'    } ); } },
             { label: 'ML 800 Band (823 - 832MHz)', click () { wc.send ( 'CHANGE_BAND', { start_freq: 823000, stop_freq: 832000, label: 'ML 800 Band (823 - 832MHz)' } ); } },
